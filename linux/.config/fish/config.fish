428f36e7f81074bd758a47a8a41dbcc3563886a6
RadConMax <RadConMax@gmail.com>
2026-06-10 15:22:15 -0700

    Create config.fish

---
+++ linux/.config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+set -gx EDITOR "nvim"
+set -gx SUDO_EDITOR "nvim"
